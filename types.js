522d4b58d552870e859f6b412f76c69b7933bf1e
Sufiane Souissi <ssouissi@lafourchette.com>
2019-10-12 19:22:53 +0200

    refactor(type): format file to be more readable

--- types.js
+++ types.js
@@ -3,68 +3,68 @@ module.exports = `
 scalar Date
 
 enum Genre {
-Action
-Adventure
-Biographical
-Catastrophic
-Comedic
-Drama
-Spy
-Fantastic
-Historical
-Horror
-Peplum
-Polar
-Scy_fy
-Thriller
-Western
+	Action
+	Adventure
+	Biographical
+	Catastrophic
+	Comedic
+	Drama
+	Spy
+	Fantastic
+	Historical
+	Horror
+	Peplum
+	Polar
+	Scy_fy
+	Thriller
+	Western
 }
 
 type Identity {
-name: String!
-dateOfBirth: Date
-country: String
+	name: String!
+	dateOfBirth: Date
+	country: String
 }
 
 type Actor {
-identity: Identity!
-appearsIn: [Movie]!
+	identity: Identity!
+	appearsIn: [Movie]!
 }
 
 type Director {
-identity: Identity!
-directed: [Movie]!
+	identity: Identity!
+	directed: [Movie]!
 }
 
 type Movie {
-title: String!
-director: Director!
-actors: [Actor!]!
-release: Date!
-synopsis: String!
-genres: [Genre!]!
-note: Int
-trailer: String
-length: Int
-poster: String
+	title: String!
+	director: Director!
+	actors: [Actor!]!
+	release: Date!
+	synopsis: String!
+	genres: [Genre!]!
+	note: Int
+	trailer: String
+	length: Int
+	poster: String
 }
 
 type Suggestion {
-created: Date!
-release: Date!
-movie: Movie!
-reason: String!
+	created: Date!
+	release: Date!
+	movie: Movie!
+	reason: String!
 }
 
 
 type Query {
-actor: Actor
-movie(movieTitle: String!): Movie
-suggestion(date: Date!): Suggestion
+	actor: Actor
+	movie(movieTitle: String!): Movie
+	suggestion(date: Date!): Suggestion
 }
 
 type Mutation {
-movies: [Movie]!
-suggestions: [Suggestion]!
+	movies: [Movie]!
+	suggestions: [Suggestion]!
 }
 `
